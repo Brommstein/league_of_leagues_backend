9cbb2e4862e59e04099b3997fd12938e22f22510
Ryan Conley <ryandeanconley2@yahoo.com>
2021-02-22 00:22:26 -0600

    possible fix?

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "inhjayoelppvqd",
-    password: "9561d42deee587ac13a0973ad2a01b2ba2d6a139cb4ce45ff49fd857de1ca2d4",
-    host: "ec2-3-95-85-91.compute-1.amazonaws.com",
-    port: 5432,
-    database: "dbr3eu1rmch5gk"
+    access: 'heroku pg:psql postgresql-aerodynamic-59043 --app league-of-leagues-backend'
+});
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res);
+    pool.end();
 });
 
 module.exports = pool;
